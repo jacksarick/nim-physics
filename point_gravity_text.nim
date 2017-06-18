318a54da5b28734509db5d6aba11c09d15e835d8
jacksarick <jacksarick@gmail.com>
2017-06-17 23:14:00 -0600

    converted to float. still broken

--- point_gravity_text.nim
+++ point_gravity_text.nim
@@ -7,25 +7,24 @@ include math
 # Constants
 var G: float = 1.0
 
-# Square root
-##Int -> Int isn't standard
-proc sqrt(a: int): int = 
-  return toInt(sqrt(toFloat(a)))
+# Squaring
+proc sqr(a: float): float =
+  return a * a
 
 ###########
 # Vectors #
 ###########
 
 # Define a vector
-type Vector = tuple[x: int, y: int]
+type Vector = tuple[x: float, y: float]
 
 # Adding vectors
 proc `+`(a: Vector, b: Vector): Vector =
   return (x: a.x + b.x, y: a.y + b.y)
 
 # Distance between vectors
-proc `<->`(a: Vector, b: Vector): int =
-  return sqrt(((a.x - b.x) ^ 2) + ((a.y - b.y) ^ 2))
+proc `<->`(a: Vector, b: Vector): float =
+  return sqrt((sqr(a.x - b.x)) + (sqr(a.y - b.y)))
 
 #########
 # World #
@@ -33,15 +32,16 @@ proc `<->`(a: Vector, b: Vector): int =
 
 # Defining an object
 ## They have position, velocity, acceleration, and mass
-type Object = tuple[p: Vector, v: Vector, a: Vector, m: int]
+type Object = tuple[p: Vector, v: Vector, a: Vector, m: float]
 
 proc gravity(a, b: Object): Vector =
   let
     force = G * ((a.m * b.m) / (a.p <-> b.p))
-    x_a: int = toInt(cos(force))
-    y_a: int = toInt(sin(force))
+    x_a: float = cos(force) * (if a.p.x > a.p.x: 1 else: -1)
+    y_a: float = sin(force) * (if a.p.y > a.p.y: 1 else: -1)
 
   # if force == inf: return (x: 0, y: 0)
+  echo((x: x_a, y: y_a))
   return (x: x_a, y: y_a)
 
 ##############
@@ -63,8 +63,8 @@ proc update_object(obj: Object): Object =
 
 # Create objects
 var
-  planet: Object = ((x: 0, y:0), (x: 0, y:0), (x: 0, y:0), 10)
-  ship: Object = ((x: 5, y:5), (x: 0, y:0), (x: 0, y:0), 1)
+  planet: Object = ((x: 0.0, y:0.0), (x: 0.0, y:0.0), (x: 0.0, y:0.0), 10.0)
+  ship: Object = ((x: 5.0, y:5.0), (x: 0.0, y:0.0), (x: 0.0, y:0.0), 1.0)
 
 echo ship
 while true:
